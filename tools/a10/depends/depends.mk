4bc1883586bd52872ba58792a90e4d32caa8c637
fred <fred@fred.fr>
2015-11-13 08:30:18 +0100

    adding v4 fpu

--- tools/a10/depends/depends.mk
+++ tools/a10/depends/depends.mk
@@ -103,7 +103,7 @@ ${RLINK_PATH} \
 -L${SDKSTAGE}/usr/lib/arm-linux-gnueabi$(HF)
 
 ifeq ($(USEARMHF), 1)
-export CFLAGS=-pipe -O3 -mfloat-abi=hard -mtune=cortex-a7 -mcpu=cortex-a7 -D__ARM_NEON__ -DALLWINNERA20
+export CFLAGS=-pipe -O3 -mfloat-abi=hard -mfpu=neon-vfpv4 -mtune=cortex-a7 -mcpu=cortex-a7 -D__ARM_NEON__ -DALLWINNERA20
 else
 export CFLAGS=-pipe -O3 -mfloat-abi=softfp -mtune=cortex-a7 -mcpu=cortex-a7 -D__ARM_NEON__ -DALLWINNERA20
 endif
