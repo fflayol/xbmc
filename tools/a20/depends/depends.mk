d70505b3b207a0147c1c79300905e45022a5e9b3
root <root@localhost.(none)>
2015-11-14 21:14:48 +0000

    removing dvdcss

--- tools/a20/depends/depends.mk
+++ tools/a20/depends/depends.mk
@@ -103,7 +103,7 @@ ${RLINK_PATH} \
 -L${SDKSTAGE}/usr/lib/arm-linux-gnueabi$(HF)
 
 ifeq ($(USEARMHF), 1)
-export CFLAGS=-pipe -O3 -mfloat-abi=hard -mfpu=neon-vfpv4 -mtune=cortex-a7 -mcpu=cortex-a7 -D__ARM_NEON__ -DALLWINNERA10
+export CFLAGS=-pipe -O3 -ffast-math -mfloat-abi=hard -mfpu=neon-vfpv4 -mtune=cortex-a7 -mcpu=cortex-a7 -D__ARM_NEON__ -DALLWINNERA10
 else
 export CFLAGS=-pipe -O3 -mfloat-abi=softfp -mtune=cortex-a7 -mcpu=cortex-a7 -D__ARM_NEON__ -DALLWINNERA10
 endif
